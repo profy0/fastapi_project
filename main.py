0769f2c2ea46afc9ea03f12b3ba585d8cd3e1d6b
Nikita Yurchik <nikita-yurchik@mail.ru>
2024-02-13 19:19:48 +0300

    new defs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,3 +15,30 @@ async def post():
 @app.put("/")
 async def put():
     return {"message": "hello from the put route"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, q: Optional[str] = None):   # or -> q: str | None = None
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.get("/items")
+async def read_items(q: str = Query(..., min_length=2, max_length=10)):
+    results = {"items": [{"item_id" : "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
